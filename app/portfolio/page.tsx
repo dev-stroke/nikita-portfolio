39bf3dd78a425a014a5685adbed283a9d367e078
EncryptArx <dev.encryptarx@gmail.com>
2025-11-17 17:14:44 +0530

    error fix

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,11 @@
+import Layout from '@/components/Layout';
+import PortfolioPageContent from '@/components/PortfolioPageContent';
+
+export default function PortfolioPage() {
+  return (
+    <Layout>
+      <PortfolioPageContent />
+    </Layout>
+  );
+}
+
